12e2dc483f3b2dcde093a88f6dac81430b61a89e
VKhushi07 <94143199+VKhushi07@users.noreply.github.com>
2022-05-09 19:27:56 -0400

    Final upload

---
+++ quiz5.js
@@ -0,0 +1,440 @@
+var quiz = 
+{
+  // (A) PROPERTIES
+  // (A1) QUESTIONS & ANSWERS
+  // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
+  data: [
+   {
+    q : "161. A person is not trespassing under the Trespass to Property Act if the owner or occupier cannot give a valid reason why they should leave",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+   {
+    q : "162. 10 code for stop transmitting",
+    o : [
+      "10-3",
+      "10-6",
+      "10-7",
+      "10-8"
+    ],
+    a : 0
+  },
+   {
+    q : "163. Which below would be considered Covert surveillance",
+    o : [
+      "Conducting surveillance from a place hidden from view",
+      "Openly following someone in plain view",
+      "Sitting outside a house on the street in plain view",
+      "None of the above"
+    ],
+    a : 0
+  },
+   {
+    q : "164. Two ways a Security guard can search an individual",
+    o : [
+      "By Consent of an individual",
+      "By Force",
+      "Upon Arrest- If you believe the person may have a weapon",
+    ],
+    a : 0
+  },
+   {
+    q : "165. Under the Trespass To Property Act an Occupier includes",
+    o : [
+      "a person who is in physical possession of premises",
+      "a person who has responsibility for and control over the condition of premises or the activities there carried on",
+      "A person who has control over persons allowed to enter the premises",
+      "All of the above"
+    ],
+    a : 3
+  },
+   {
+    q : "166. Under the Liquor License Act you can only arrest not remove",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+   {
+    q : "166. Under Occupational Health and Safety workers now have the right to refuse work if they have a reason to believe they are in danger from workplace violence",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "167. Section 10 (a) of the Charter Of Rights and Freedoms (arrest or detention) relates to",
+    o : [
+      "A person needs to be advised of their right to council",
+      "A person has the right to be informed promptly of the reasons for the arrest or detention",
+      "Search or Seizure",
+      "Everyone has the right to life, liberty and security"
+    ],
+    a : 1
+  },
+   {
+    q : "168. It is an employee's responsibility to report violations of workplace safety",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "169. Positional asphyxia may occur when a person is being physically restrained. This condition can lead to",
+    o : [
+      "Injury",
+      "Death",
+      "Violent behavior",
+      "All of the above"
+    ],
+    a : 3
+  },
+   {
+    q : "170. Anyone can make an arrest if they find someone committing a CRIMINAL OFFENCE",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+   {
+    q : "171. The six core steps to containing evidence are",
+    o : [
+      "Collect, Secure, Preserve, Identify, Ensure Continuity, Log",
+      "Collect, Secure, Preserve, Store , Ensure Continuity, Log",
+      "Collect, Secure, Preserve, Identify, Ensure Continuity, Court",
+      "Collect, Package, Preserve, Identify, Ensure Continuity, Log"
+    ],
+    a : 0
+  },
+   {
+    q : "171. While on patrol in a mall you come across a disabled person having problems with their wheelchair. What action should you take ?",
+    o : [
+      "Advise the person they ought to buy a new wheelchair",
+      "Push them off to the side to protect them from injury",
+      "Walk the other way so that you do not get sued",
+      "Offer assistance to the person"
+    ],
+    a : 3
+  },
+   {
+    q : "172. Being aware of one's surroundings is called situational awareness",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "173. Which of the following is not a fundamental freedom as stated Under Section 2 of the Charter Of Rights And Freedoms",
+    o : [
+      "freedom of conscience and religion",
+      "freedom of thought",
+      "freedom of association",
+      "freedom to travel"
+    ],
+    a : 3
+  },
+   {
+    q : "174. A Security guard may assist a Landlord in evicting a tenant from an apartment",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+   {
+    q : "175. The RCMP are",
+    o : [
+      "Provincial Police",
+      "Federal Police",
+      "Municipal Police",
+      "Regional Police"
+    ],
+    a : 1
+  },
+   {
+    q : "176. A Security guard while on duty should",
+    o : [
+      "Not patrol using the same predictable routine",
+      "Be proactive",
+      "Be reactive",
+      "All of the above"
+    ],
+    a : 3
+  },
+   {
+    q : "177. Under the Criminal Code Of Canada, when a fire is deliberately set, what is the name of the offence called",
+    o : [
+      "Theft",
+      "Mischief",
+      "Arson",
+      "None of the above"
+    ],
+    a : 2
+  },
+   {
+    q : "178. Remember most fire alarms are false and should be treated as such",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+   {
+    q : "179. An Indictable offence under the Criminal Code is a serious criminal offence",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "180. Upon an arrest of an individual a Security guard or a Citizen must",
+    o : [
+      "Take a cautioned statement",
+      "Handcuff the individual",
+      "Search the individual for weapons",
+      "Turn the individual over to police"
+    ],
+    a : 3
+  },
+   {
+    q : "181. WHMIS Class B Includes",
+    o : [
+      "Compressible Gas",
+      "Flammable and Combustible Liquids",
+      "Oxidizing Material",
+      "Poisonous and Infectious Material"
+    ],
+    a : 1
+  },
+   {
+    q : "182. Historical legal precedents is the basis of Common Law",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "183. Evidence that relates to a series of facts other than the particular fact sought to be proved",
+    o : [
+      "Circumstantial evidence",
+      "Similar fact evidence",
+      "Documentary evidence",
+      "None of the above"
+    ],
+    a : 0
+  },
+   {
+    q : "184. What is the best definition of the word prejudice?",
+    o : [
+      "A preconceived belief, opinion, or judgment made without ascertaining the facts of a case",
+      "Calling someone names",
+      "Making up lies about a person not knowing what they are really like",
+      "None of the above"
+    ],
+    a : 0
+  },
+   {
+    q : "185. A manual fire alarm can be activated by",
+    o : [
+      "Using a Pull Station",
+      "Calling 911",
+      "A large amount of smoke",
+      "Breaking the glass of a fire extinguisher casing"
+    ],
+    a : 0
+  },
+   {
+    q : "186. If there is a fire alarm on your site, what would be your best course of action",
+    o : [
+      "Evacuate everyone in an orderly fashion",
+      "Call the fire department to confirm they will be attending",
+      "Refer to your emergency measures plan",
+      "See if you can schedule your lunch break so you can watch all the fire trucks arrive"
+    ],
+    a : 2
+  },
+  {
+    q : "187. 10 code for out of service",
+    o : [
+      "10-7",
+      "10-19",
+      "10-20",
+      "10-21"
+    ],
+    a : 0
+  },
+   {
+    q : "189. A Security guard has no authority in Ontario, under the Liquor Licence Act, to arrest an individual for Failing to Leave a bar after being told to do so",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "190. Approximately what percent of communication is verbal",
+    o : [
+      "1%",
+      "14%",
+      "7%",
+      "35%"
+    ],
+    a : 2
+  },
+  {
+    q : "191. If you were charged with an indictable offense in Canada you would be entitled to",
+    o : [
+      "Be heard by a Judge sitting alone",
+      "Be heard by a Judge and Jury",
+      "A preliminary hearing",
+      "All of the above"
+    ],
+    a : 3
+  },
+   {
+    q : "192. The PSISA does NOT apply to persons who receive remuneration for work that consists primarily of providing an armoured vehicle service",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "193. As a Security guard what is the purpose for Patrol",
+    o : [
+      "To keep yourself awake during an 8 hour shift",
+      "To establish a presence and to deter potential offenders",
+      "So the client thinks you are actually doing something",
+      "To see if other Security guards are violating the PSISA code of conduct"
+    ],
+    a : 1
+  },
+   {
+    q : "194. Conditional Discharge is when the conditions set by the court are satisfied, the discharge becomes absolute",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+  {
+    q : "195. As a Security guard one of your duties is to assist Landlords in removing tenants",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+  {
+    q : "196. A witness who is giving testimony is giving what type of evidence",
+    o : [
+      "Direct",
+      "Trace",
+      "Similar fact",
+      "Circumstantial"
+    ],
+    a : 0
+  }
+  ],
+
+
+  // (A2) HTML ELEMENTS
+  hWrap: null, // HTML quiz container
+  hQn: null, // HTML question wrapper
+  hAns: null, // HTML answers wrapper
+
+  // (A3) GAME FLAGS
+  now: 0, // current question
+  score: 0, // current score
+
+  // (B) INIT QUIZ HTML
+  init: () => {
+    // (B1) WRAPPER
+    quiz.hWrap = document.getElementById("quizWrap");
+
+    // (B2) QUESTIONS SECTION
+    quiz.hQn = document.createElement("div");
+    quiz.hQn.id = "quizQn";
+    quiz.hWrap.appendChild(quiz.hQn);
+
+    // (B3) ANSWERS SECTION
+    quiz.hAns = document.createElement("div");
+    quiz.hAns.id = "quizAns";
+    quiz.hWrap.appendChild(quiz.hAns);
+
+    // (B4) GO!
+    quiz.draw();
+  },
+
+  // (C) DRAW QUESTION
+  draw: () => {
+    // (C1) QUESTION
+    quiz.hQn.innerHTML = quiz.data[quiz.now].q;
+    
+
+    // (C2) OPTIONS
+    quiz.hAns.innerHTML = "";
+    for (let i in quiz.data[quiz.now].o) {
+      let radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "quiz";
+      radio.id = "quizo" + i;
+      quiz.hAns.appendChild(radio);
+      let label = document.createElement("label");
+      label.innerHTML = quiz.data[quiz.now].o[i];
+      label.setAttribute("for", "quizo" + i);
+      label.dataset.idx = i;
+      label.addEventListener("click", () => { quiz.select(label); });
+      quiz.hAns.appendChild(label);
+    }
+  },
+
+  // (D) OPTION SELECTED
+  select: (option) => {
+    // (D1) DETACH ALL ONCLICK
+    let all = quiz.hAns.getElementsByTagName("label");
+    for (let label of all) {
+      label.removeEventListener("click", quiz.select);
+    }
+
+    // (D2) CHECK IF CORRECT
+    let correct = option.dataset.idx == quiz.data[quiz.now].a;
+    if (correct) {
+      quiz.score++;
+      option.classList.add("correct");
+    } else {
+      option.classList.add("wrong");
+    }
+
+    // (D3) NEXT QUESTION OR END GAME
+    quiz.now++;
+    setTimeout(() => {
+      if (quiz.now < quiz.data.length) 
+      { 
+        quiz.draw(); 
+      }
+      else {
+        quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
+        quiz.hAns.innerHTML = "";
+      }
+    }, 1000);
+  },
+
+  // (E) RESTART QUIZ
+  reset : () => {
+    quiz.now = 0;
+    quiz.score = 0;
+    quiz.draw();
+  }
+};
+window.addEventListener("load", quiz.init);
